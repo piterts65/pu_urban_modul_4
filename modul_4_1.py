c5d71a4eab3d4ab8a64d16c211fad7a4e7176df8
pierts65 <piterts65@gmail.com>
2024-09-11 12:34:49 +0500

    end

---
+++ modul_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divide as t_m
+from fake_math import divide as f_m
+
+result1 = f_m(69, 3)
+result2 = f_m(3, 0)
+result3 = t_m(49, 7)
+result4 = t_m(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
